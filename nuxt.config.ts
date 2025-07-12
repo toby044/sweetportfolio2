e43f0aa36c37f8e9c6ff92bfbcac7bae10904a9c
Tobias Olesen <62332472+toby044@users.noreply.github.com>
2025-07-12 20:15:53 +0200

    go

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-  ],
-  devtools: { enabled: true },
-  future: {
-    compatibilityVersion: 4,
-  },
-  compatibilityDate: '2024-04-03',
-})
+    modules: ["@nuxt/content"],
+    devtools: { enabled: true },
+    future: {
+        compatibilityVersion: 4,
+    },
+    content: {
+        preview: {
+            api: "https://api.nuxt.studio",
+        },
+    },
+    compatibilityDate: "2024-04-03",
+});
